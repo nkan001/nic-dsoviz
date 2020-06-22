91179f2ee9eb8b0639bbc3ecc468061f5eac722a
nicolekan28 <nicolekan28@yahoo.com.sg>
2020-06-22 17:40:38 +0800

    d3 area chart added

--- src/pages/charts/d3Area.vue
+++ src/pages/charts/d3Area.vue
@@ -1,52 +1,86 @@
 <template>
-  <q-page class="flex flex-center">
-    <svg width="500" height="270">
-      <g style="transform: translate(0, 10px)">
-        <path :d="line" />
-      </g>
-    </svg>
+  <q-page>
+    <div id="app">
+      <h1>Area Chart</h1>
+      <responsive-area-chart
+        @select="onSelect"
+        :data="data"
+        :ceil="max"
+        class="area-chart" />
+      <div class="content">
+        <h3>Selected Value: {{currentValue}}</h3>
+        <div>
+          <label>Min Value: </label>
+          <input v-model="min" />
+        </div>
+        <div>
+          <label>Max Value: </label>
+          <input v-model="max" />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import * as d3 from 'd3'
+import ResponsiveAreaChart from 'components/d3AreaChart'
+import generateData from './randomData'
+
 export default {
-  name: 'vue-line-chart',
+  name: 'D3Area',
   data () {
     return {
-      data: [99, 71, 78, 25, 36, 92],
-      line: ''
+      data: [],
+      chartWidth: 0,
+      currentValue: null,
+      itemCount: 25,
+      min: 10,
+      max: 100
     }
   },
   mounted () {
-    this.calculatePath()
+    setInterval(() => {
+      this.data = generateData(this.itemCount,
+        parseInt(this.min, 10),
+        parseInt(this.max, 10))
+    }, 2000)
   },
   methods: {
-    getScales () {
-      const x = d3.scaleTime().range([0, 430])
-      const y = d3.scaleLinear().range([210, 0])
-      d3.axisLeft().scale(x)
-      d3.axisBottom().scale(y)
-      x.domain(d3.extent(this.data, (d, i) => i))
-      y.domain([0, d3.max(this.data, d => d)])
-      return { x, y }
-    },
-    calculatePath () {
-      const scale = this.getScales()
-      const path = d3.line()
-        .x((d, i) => scale.x(i))
-        .y(d => scale.y(d))
-      this.line = path(this.data)
+    onSelect (value) {
+      this.currentValue = value
     }
+  },
+  components: {
+    ResponsiveAreaChart
   }
 }
 </script>
 
+<style lang="sass">
+body
+  margin: 0
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  margin-top: 60px
+</style>
+
 <style lang="sass" scoped>
-svg
-  margin: 25px;
-path
-  fill: none
-  stroke: #76BF8A
-  stroke-width: 3px
+h1, h3
+    color: white
+
+h1, .content
+  margin-left: 20px
+
+label
+  color: white
+  display: inline-block
+  width: 150px
+
+.area-chart
+  height: 200px
+
 </style>
